14215ccc1672a52baeecf93847076b1be01cacf3
Ian Howson <ian@mutexlabs.com>
2018-09-05 11:59:46 -0700

    Fiddling

--- src/api_client/apiClientDeploy.js
+++ src/api_client/apiClientDeploy.js
@@ -20,10 +20,10 @@ function listener() {
  }
 }
 
-export var serverAddress = 'http://ownphotos'
+export var serverAddress = 'http://localhost:8851'
 
 export var Server = axios.create({
-  baseURL: 'http://ownphotos/api/',
+  baseURL: 'http://localhost:8851/api/',
   headers: {
     'Content-Type': 'application/json'
   },
